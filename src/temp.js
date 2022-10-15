292afbf4371e05b50cbf8a9af4f94d2dd8dd540d
Prau-lab <prayushthapa12@gmail.com>
2022-10-15 22:17:25 +0545

    final changes

---
+++ src/temp.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import Weathercard from "./weathercard";
+import "./style.css";
+
+//
+
+const Temp = () => {
+const [searchValue, setSearchValue] = React.useState("Kathmandu");
+const [tempInfo, setTempInfo] = React.useState({})
+
+//async use gareko bela try and catch chaincha.
+const getWeatherInfo = async () => {
+  try {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=c1cb1b23f06a972c1a3d6d31da0ed3c1`
+    const res = await fetch(url);
+    const data = await res.json();
+    const { temp, humidity, pressure } = data.main; //desctructuring the data
+    const { main:weathermood } = data.weather[0];
+    const { name } = data;
+    const { speed } = data.wind;
+    const { country, sunset } = data.sys;
+
+    const myNewWeatherInfo = {
+      temp, 
+      humidity, 
+      pressure, 
+      weathermood, 
+      name, 
+      speed, 
+      country, 
+      sunset,
+    };
+    setTempInfo(myNewWeatherInfo);
+
+  } catch (error) {
+    
+  }
+}
+
+React.useEffect(() => {
+  getWeatherInfo();
+}, [])
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input type="search"
+            placeholder='search...'
+            autoFocus
+            id='search'
+            className='searchTerm' 
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}/>
+          <button className="searchButton" 
+          type='button' 
+          onClick={getWeatherInfo}>Search</button>
+        </div>
+      </div>
+
+      <Weathercard tempInfo = {tempInfo}/>
+    </>
+  )
+}
+
+export default Temp;
